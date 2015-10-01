db867b39c26be51f0ecea83dff820a355966b09c
barkford <emoquestrian@gmail.com>
2015-10-01 02:32:29 -0400

    hacked around, need help

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,27 @@
+#!/usr/bin/env ruby
+# coding: utf-8
+
+require 'rubygems'
+
+require 'open-uri' # http://ruby-doc.org/stdlib-2.2.2/libdoc/open-uri/rdoc/OpenURI.html
+
 class PagesController < ApplicationController
   def home
+    #
+    #
+    # Source pdf  http://publicdocs.courts.mi.gov:81/Opinions/Final/SCt/20150729_S149073_83_01_Lockridge-OP.pdf
+    def PDFsource
+
+      receiver = PDF::Reader::RegisterReceiver.new
+      @PDFsource ||= File.dirname("http://publicdocs.courts.mi.gov:81/Opinions/Final/SCt/20150729_S149073_83_01_Lockridge-OP.pdf")
+      end
+
+    def PDFmeta
+    @PDFmeta ||= PDF::Reader.open(@PDFsource) do |reader|
+      puts reader.info.inspect
+      puts reader.metadata.inspect
+      end
+    end
+
   end
 end
