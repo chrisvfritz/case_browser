2eea8d7c54a0030586ca9334bd963348da7f0d89
barkford <emoquestrian@gmail.com>
2015-10-07 11:53:33 -0400

    changed File.dirname to open| now showing data on localhost

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,27 +1,14 @@
-#!/usr/bin/env ruby
-# coding: utf-8
-
-require 'rubygems'
-
 require 'open-uri' # http://ruby-doc.org/stdlib-2.2.2/libdoc/open-uri/rdoc/OpenURI.html
+require 'pdf-reader'
 
 class PagesController < ApplicationController
   def home
-    #
-    #
-    # Source pdf  http://publicdocs.courts.mi.gov:81/Opinions/Final/SCt/20150729_S149073_83_01_Lockridge-OP.pdf
-    def PDFsource
+    receiver = PDF::Reader::RegisterReceiver.new
+    pdf_source = open("http://publicdocs.courts.mi.gov:81/Opinions/Final/SCt/20150729_S149073_83_01_Lockridge-OP.pdf")
 
-      receiver = PDF::Reader::RegisterReceiver.new
-      @PDFsource ||= File.dirname("http://publicdocs.courts.mi.gov:81/Opinions/Final/SCt/20150729_S149073_83_01_Lockridge-OP.pdf")
-      end
-
-    def PDFmeta
-    @PDFmeta ||= PDF::Reader.open(@PDFsource) do |reader|
-      puts reader.info.inspect
-      puts reader.metadata.inspect
-      end
+    PDF::Reader.open(pdf_source) do |reader|
+      @pdf_info = reader.info.inspect
+      @pdf_metadata =  reader.metadata.inspect
     end
-
   end
 end
